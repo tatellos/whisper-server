29644770425615f389e58b36e3c54e57c9a84784
Mathias Arens <mathias@arens.ma>
2023-02-26 15:14:25 +0100

    Proxy the call to the language model through the whisper backend, so that there are no problems with CORS etc

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ import uuid
 import aiofiles as aiofiles
 import whisper
 from fastapi import FastAPI, UploadFile
+import requests
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 from model import model
 
 app = FastAPI()
 os.makedirs("transcriptions", exist_ok=True)
 
+
 @app.get("/")
 async def main():
     return HTMLResponse(open("test.html").read())
-
 
 
 app.mount("/transcriptions", StaticFiles(directory="transcriptions"), name="transcriptions")
@@ -35,6 +37,18 @@ async def create_upload_file(sound: UploadFile):
     return StreamingResponse(text, media_type="text/plain")
 
 
+class TextInJson(BaseModel):
+    text: str
+
+@app.post("/languagemodel")
+async def proxy_to_languagemodel(text: TextInJson):
+    input_text = text.text
+    print("Proxying to languagemodel", input_text)
+    # send the input text in a form to the languagemodel
+    response = requests.post("http://localhost:8000", data={"text": input_text}).text
+    return TextInJson(text=response)
+
+
 def transcribe(file_name, original_name) -> str:
     print("using tmp file", file_name)
     timestamp = dt.datetime.now(dt.timezone.utc).strftime("%Y-%m-%dZ%H-%M-%S")
@@ -49,4 +63,4 @@ def transcribe(file_name, original_name) -> str:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
